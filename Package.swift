4a5fa873cbd4a0618b94d0acb7b39a83d7a14817
michael.shen <michael.shen@104.com.tw>
2024-12-20 15:36:35 +0800

    used ssh

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TestPackage",
-            url: "https://github.com/cac-michael/KMMPackage/releases/download/Framework/Shared.xcframework.zip",
+            url: "git@github.com:cac-michael/KMMPackage/releases/download/Framework/Shared.xcframework.zip",
             checksum:"5a8eb0b050e7b32cd6a8046d0037e56d82b189c154a85ef760205f7ebc7c4144")
     ]
 )
